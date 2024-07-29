8f2e1defd2f8727259e4295573760d969acfbbb4
ArhaanAli04 <arhaan.ali2004@gmail.com>
2024-07-29 21:40:02 +0530

    js

--- new.js
+++ new.js
@@ -6,5 +6,19 @@ const list=document.querySelector("#fruits");
 const listItem=document.createElement("li");
 listItem.textContent="mango";
 list.append(listItem);
+/*
+let count =0;
+document.getElementById("1").onclick=function(){
+    if(document.getElementById("add")){
+        count=count +1;
+    }
+    else if(document.getElementById("subtract")){
+        count =count-1;
+    }
+    document.getElementById("result").innerHTML=count;
+
+} */
 
 
+//cookies:
+console.log(navigator.cookieEnabled);    
